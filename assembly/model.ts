b5be293c7a0b778d4e31dca7a4af5293ab6f69ec
alimuratkuslu <alimuratkuslu@gmail.com>
2022-04-10 15:11:44 +0300

    Created model.ts file

---
+++ assembly/model.ts
@@ -0,0 +1,19 @@
+import{PersistentUnorderedMap, math} from "near-sdk-as";
+
+export const map = new PersistentUnorderedMap<u32, Transaction>("map");
+
+@nearBindgen
+export class Transaction {
+
+    id: u32;
+    transaction: u32;
+    message: string;
+    done: bool;
+
+    constructor(transaction: u32, message: string){
+        this.id = math.hash32<string>(message);
+        this.message = message;
+        this.transaction = transaction;
+        this.done = false;
+    }
+}
