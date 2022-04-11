ffc7110f60bfdbd76f0e8afe39e24fe9cb891ccc
alimuratkuslu <alimuratkuslu@gmail.com>
2022-04-11 19:14:39 +0300

    Created findById function

--- assembly/index.ts
+++ assembly/index.ts
@@ -1,5 +1,9 @@
 import { Transaction } from "./model";
 
 export function create(transaction:u32, message:string): Transaction {
-  return Transaction.insert(transaction, message);
+  return Transaction.insert(transaction, message);  
+}
+
+export function getById(id: u32): Transaction {
+  return Transaction.findTxById(id);
 }
