eba2db78ab2f18d623f31636d53c23d4c8dfe060
alimuratkuslu <alimuratkuslu@gmail.com>
2022-04-27 12:40:02 +0300

    Changed script

--- scripts/4.update-movie.sh
+++ scripts/4.update-movie.sh
@@ -8,4 +8,5 @@ echo 'About to call update() on the contract'
 echo sudo near call \$CONTRACT update '{"id": '$MOVIEID', "updates":{"name": "Harry Potter", "price": 13.7, "rating": 9.2, "duration": 120, "type": "Fiction", "stock": 20} }' --accountId \$CONTRACT 
 echo
 echo \$CONTRACT is $CONTRACT
+echo \$MOVIEID is $MOVIEID
 sudo near call $CONTRACT update '{"id": '$MOVIEID', "updates":{"name": "Harry Potter", "price": 13.7, "rating": 9.2, "duration": 120, "type": "Fiction", "stock": 20} }' --accountId $CONTRACT 
