8ee121ef067b9c871f90bc53458502f05f809a7c
alimuratkuslu <alimuratkuslu@gmail.com>
2022-04-27 12:07:21 +0300

    Changed script

--- scripts/2.create-movie.sh
+++ scripts/2.create-movie.sh
@@ -5,7 +5,14 @@ set -e
 
 echo
 echo 'About to call create() on the contract'
-echo sudo near call \$CONTRACT create '{"name":"Inception", "price": 10.5, "rating": 8.8, "duration": 180, "type": "Action", "stock": 10}' --account_id \$CONTRACT 
+echo sudo near call \$CONTRACT create '{"name":"Inception", "price": 10.5, "rating": 8.8, "duration": 180, "type": "Action", "stock": 10}' --accountId \$CONTRACT 
 echo
 echo \$CONTRACT is $CONTRACT
 sudo near call $CONTRACT create '{"name":"Inception", "price": 10.5, "rating": 8.8, "duration": 180, "type": "Action", "stock": 10}' --accountId $CONTRACT
+
+echo --------------------------------------------
+echo run the following commands
+echo
+echo 'export MOVIEID=<movie_id>'
+echo
+echo
